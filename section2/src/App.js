989e12e2cef9dbcaecdead413392aa05cbbbb78d
codecamjam <camerontaghlabi@gmail.com>
2021-09-07 15:21:33 -0400

    l21 removing data-test attributes for production pt1

--- section2/src/App.js
+++ section2/src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div data-test="component-app"></div>;
+  return (
+    <div data-test="component-app">
+      <h1>App</h1>
+    </div>
+  );
 }
 
 export default App;
