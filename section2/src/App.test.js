7d224031c4f57597fc2bd26745606ec794c264f4
codecamjam <camerontaghlabi@gmail.com>
2021-09-07 15:57:04 -0400

    l23 DRY refactor

--- section2/src/App.test.js
+++ section2/src/App.test.js
@@ -4,32 +4,49 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Factory function to create a ShallowWrapper for the
+ * App component.
+ * @function setup
+ * @returns {ShallowWrapper: A new wrapper that wraps the found nodes.}
+ */
+const setup = () => shallow(<App />);
+
+/**
+ * Factory function to find the data-test attribute from an element
+ * @function findByTestAttr
+ * @param {*} wrapper
+ * @param {*} val
+ * @returns {ShallowWrapper}
+ */
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
 test("renders without error", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
 });
 
 test("renders increment button", () => {
-  const wrapper = shallow(<App />);
-  const button = wrapper.find("[data-test='increment-button']");
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "increment-button");
   expect(button.length).toBe(1);
 });
 
 test("renders counter display", () => {
-  const wrapper = shallow(<App />);
-  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
   expect(counterDisplay.length).toBe(1);
 });
 
 test("counter display starts at 0", () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   const appComponent = wrapper.find("[data-test='component-app']");
   expect(appComponent.length).toBe(1);
 });
 
 test("clicking button increments counter display", () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   const appComponent = wrapper.find("[data-test='component-app']");
   expect(appComponent.length).toBe(1);
 });
