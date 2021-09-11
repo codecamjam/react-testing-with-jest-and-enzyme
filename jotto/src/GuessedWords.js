5aa8689b9f357e049803d279e0cb6691c3b3e3b8
codecamjam <camerontaghlabi@gmail.com>
2021-09-11 19:03:15 -0400

    l48 code for  no words guessed

--- jotto/src/GuessedWords.js
+++ jotto/src/GuessedWords.js
@@ -1,7 +1,13 @@
 import PropTypes from 'prop-types';
 
 const GuessedWords = (props) => {
-  return <div>dfsfsdf</div>;
+  let contents;
+  if (props.guessedWords.length === 0) {
+    contents = (
+      <span data-test='guess-instructions'>Try to guess the secret word!</span>
+    );
+  }
+  return <div data-test='component-guessed-words'>{contents}</div>;
 };
 
 GuessedWords.propTypes = {
