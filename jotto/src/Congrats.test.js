bac9e448b208689885c6235bc662c181de39be6e
codecamjam <camerontaghlabi@gmail.com>
2021-09-10 21:50:36 -0400

    l40 OPTIONAL default props in setup()

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -6,14 +6,18 @@ import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const defaultProps = { success: false };
+
 /**
  * Factory function to create a ShallowWrapper for the congrats component
  * @function setup
  * @param {object} props - Component props specific to this setup
  * @returns {ShallowWrapper}
  */
-const setup = (props = {}) => shallow(<Congrats {...props} />);
-
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  shallow(<Congrats {...setupProps} />);
+};
 test("renders without error", () => {
   const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
