65f34545b8021b3214a03e7fd3cf28853e6cbf16
codecamjam <camerontaghlabi@gmail.com>
2021-09-13 16:51:35 -0400

    l64 set up functional tests

---
+++ jotto/src/guessedWord.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import App from './App';
+import { findByTestAttr } from '../test/testUtils';
+
+/**
+ * Create a wrapper with specified initial conditions
+ * then submit a guessed word of 'train'
+ * @function
+ *
+ * @param {object} state - Initial conditions.
+ * @returns {Wrapper} - Enzyme wrapper of mounted App component
+ */
+const setup = (state = {}) => {
+  // TODO: apply state
+  const wrapper = mount(<App />);
+
+  //add value to input box
+  const inputBox = findByTestAttr(wrapper, 'input-box');
+  inputBox.simulate('change', { target: { value: 'train' } });
+
+  //simulate click on submit button
+  const submitButton = findByTestAttr(wrapper, 'submit-button');
+  submitButton.simulate('click', { preventDefault() {} });
+};
+
+describe('no words guessed', () => {});
+
+describe('some words guessed', () => {});
+
+describe('guess secret word', () => {});
