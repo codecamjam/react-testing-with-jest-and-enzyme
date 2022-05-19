7711879ed4a1cfba759ae2518e18e88cffc6b81e
codecamjam <camerontaghlabi@gmail.com>
2022-05-18 22:06:51 -0400

    l106 update moxios tests for redux

--- jotto/src/actions/index.test.js
+++ jotto/src/actions/index.test.js
@@ -1,5 +1,6 @@
 import moxios from 'moxios';
 import { getSecretWord } from './';
+import { storeFactory } from '../../test/testUtils';
 
 /*
  1. FOR ASYNC FUNCTIONS, MAKE SURE YOU RETURN 
@@ -23,13 +24,14 @@ describe('getSecretWord', () => {
   });
 
   test('secretWord is returned', () => {
+    const store = storeFactory();
     moxios.wait(() => {
       const request = moxios.requests.mostRecent();
       request.respondWith({ status: 200, response: 'party' });
     });
 
-    //update to test app in Redux / context sections
-    return getSecretWord().then((secretWord) => {
+    return store.dispatch(getSecretWord()).then(() => {
+      const secretWord = store.getState().secretWord;
       expect(secretWord).toBe('party');
     });
   });
